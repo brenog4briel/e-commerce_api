994ff429b206112355d5e60e9f729b2ecfbce7e8
brenog4briel <breno-112011@hotmail.com>
2024-06-11 17:10:06 -0300

    feat: implementacao do nodemail para recuperacao de senhas

--- src/routes/usuario.routes.ts
+++ src/routes/usuario.routes.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify";
 import { UsuarioData, Usuario } from "../interfaces/usuario.interface";
 import { UsuarioUseCase } from "../usecases/usuario.usecase";
 import { AuthService } from "../auth/auth.usecase";
+import nodemailer from "nodemailer"
 
 export async function UsuarioRoutes(fastify:FastifyInstance) {
 
@@ -41,5 +42,31 @@ export async function UsuarioRoutes(fastify:FastifyInstance) {
         } catch (error) {
             reply.send(error)
         }
+    })
+
+    fastify.get<{Params:{email:string}}>('/recuperacao/:email', async(req, reply) => {
+        const transporter = nodemailer.createTransport({
+            host: process.env.NODEMAIL_HOST,
+            port: Number(process.env.NODEMAIL_PORT),
+            secure: false, // use SSL
+            auth: {
+                user: process.env.NODEMAIL_USER,
+                pass: process.env.NODEMAIL_PASS,
+            }
+    });
+
+        let email = req.params.email
+        try {
+            let message = await transporter.sendMail({
+            from: 'brenosacerdote@academico.ufs.br',
+            to: email,
+            subject: 'Sending Email using Node.js',
+            text: 'That was easy!',
+            html:`<h1>Email enviado para ${email}</h1>`
+        })
+        } catch (error) {
+            reply.send(error)
+        }
+        
     })
 }
