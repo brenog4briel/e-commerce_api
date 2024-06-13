3bf4070c73dffd5c75af60c1f9f6de02bc798275
brenog4briel <breno-112011@hotmail.com>
2024-06-13 19:41:56 -0300

    feat: teste deploy

--- src/upload/upload.ts
+++ src/upload/upload.ts
@@ -1,17 +1,6 @@
 import Multer from "fastify-multer"
 import { UsuarioUseCase } from "../usecases/usuario.usecase";
 
-interface teste {
-  fieldname: string,
-  originalname: string,
-  encoding: string,
-  mimetype: string,
-  destination: string,
-  filename: string,
-  path: string,
-  size: number
-}
-
 const usuarioUseCase = new UsuarioUseCase()
 
 const ROOT_PATH = process.env.ROOT_PATH || __dirname
