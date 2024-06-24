a1466c771d682b3a669fc01ee9688fedb9698a64
brenog4briel <breno-112011@hotmail.com>
2024-06-24 16:59:32 -0300

    teste rota render

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -5,19 +5,19 @@ export async function authMiddleware(req:FastifyRequest,reply:FastifyReply) {
     
     const authService = new AuthService();
     // Postman
-    // const {token}  = req.headers;
+    const {token}  = req.headers;
     
-    // if (!token) {throw new Error("Você não tem permissão para acessar essa rota!")}
-    // const isAuth = await authService.verifyToken(token);
+    if (!token) {throw new Error("Você não tem permissão para acessar essa rota!")}
+    const isAuth = await authService.verifyToken(token);
 
     //Render
 
-    const tokenReceived : string = req.headers.authorization!;
-    if (!tokenReceived) {throw new Error("Você não tem permissão para acessar essa rota!")}
-    const tokenFormatted = tokenReceived.slice(7,tokenReceived.length)
+    // const tokenReceived : string = req.headers.authorization!;
+    // if (!tokenReceived) {throw new Error("Você não tem permissão para acessar essa rota!")}
+    // const tokenFormatted = tokenReceived.slice(7,tokenReceived.length)
 
-    if (!tokenFormatted) {reply.status(401).send({message:"Usuário não autorizado!"})}
+    // if (!tokenFormatted) {reply.status(401).send({message:"Usuário não autorizado!"})}
     
-    const isAuth = await authService.verifyToken(tokenFormatted);
+    // const isAuth = await authService.verifyToken(tokenFormatted);
 
     }
