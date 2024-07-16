a250420636d7a67d2cd10a776f08b419ed13f6be
brenog4briel <breno-112011@hotmail.com>
2024-07-16 10:47:20 -0300

    feat: finalização

--- src/routes/pedido_de_compra.routes.ts
+++ src/routes/pedido_de_compra.routes.ts
@@ -46,13 +46,13 @@ export async function Pedido_de_compraRoutes(fastify:FastifyInstance) {
         }
     })
 
-    fastify.delete<{Body:{pedido_de_compra_id:string}}>("/remove-all",async(req,reply) => { 
-        const {pedido_de_compra_id} = req.body;
+    fastify.delete<{Params:{pedido_de_compra_id:string}}>("/remove-all",async(req,reply) => { 
+        const {pedido_de_compra_id} = req.params;
         try {
             const result = await pedido_de_compra.deletePedido(pedido_de_compra_id);
             return reply.send(result)
         } catch (error) {
-            throw new Error("Houve um erro ao limpar o pedido de compra")
+            throw new Error("Houve um erro ao apagar o pedido de compra")
 
         }
     })
